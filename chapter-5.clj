95595f9f043d16ceab9625c2c755715502072952
Pierre-Emile Ferron <peferron@gmail.com>
2019-07-07 17:55:53 -0700

    Finish chapter 5 exercises

--- chapter-5.clj
+++ chapter-5.clj
@@ -26,3 +26,32 @@
 
 (def test-fn (my-comp inc *))
 (test-fn 1 2)
+
+; 3
+
+(defn my-assoc-in
+  [m [k & ks] v]
+  (if ks
+    (assoc m k (my-assoc-in (get m k) ks v))
+    (assoc m k v)))
+
+(my-assoc-in {:a {:b 0 :c 1}} [:a :d] 2)
+(my-assoc-in {} [:a :b] 0)
+
+; 4
+
+(update-in {:a {:b 0 :c 1}} [:a :c] inc)
+(update-in {:a {:b 0 :c 1}} [:a :c] * 2)
+(update-in {} [:a :b] (fn [_] 0))
+
+; 5
+
+(defn my-update-in
+  [m [k & ks] f & args]
+  (if ks
+    (assoc m k (apply my-update-in (get m k) ks f args))
+    (apply update m k f args)))
+
+(my-update-in {:a {:b 0 :c 1}} [:a :c] inc)
+(my-update-in {:a {:b 0 :c 1}} [:a :c] * 2)
+(my-update-in {} [:a :b] (fn [_] 0))
