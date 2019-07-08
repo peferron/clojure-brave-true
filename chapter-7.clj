3e0c94f1de33d3bcd0897b567633da319aeeab34
Pierre-Emile Ferron <peferron@gmail.com>
2019-07-08 01:30:02 -0700

    Add chapter 7 exercises

---
+++ chapter-7.clj
@@ -0,0 +1,48 @@
+; 1
+
+(def movie (list (read-string "println") '(str "Pierre-Emile Ferron" ": " "Gattaca")))
+(eval movie)
+
+; 2
+
+(def infixed '(1 + 3 * 4 - 5))
+
+(defn priority
+  [item]
+  (case item
+    (+ -) 1
+    (* /) 2
+    0))
+
+(map priority infixed)
+
+(defn max-priority
+  [items]
+  (apply max (map priority items)))
+
+(max-priority infixed)
+
+(defn simplify
+  ([items]
+   (simplify items (max-priority items)))
+  
+  ([items max-p]
+   (let [[left-operand operator right-operand & remaining] items]
+     (if (= (priority operator) max-p)
+       (apply list (list operator left-operand right-operand) remaining)
+       (concat (list left-operand operator) (simplify (cons right-operand remaining) max-p))))))
+
+(simplify '(1 + 3))
+(simplify '(1 + 3 * 4))
+(simplify '(1 + 3 * 4 - 5))
+(simplify '(1 + (* 3 4) - 5))
+(simplify '((+ 1 (* 3 4)) - 5))
+
+(defn infix
+  [items]
+  (if (second items)
+    (recur (simplify items))
+    (first items)))
+
+(infix infixed)
+(eval (infix infixed))
